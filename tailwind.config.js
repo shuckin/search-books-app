aad6adb439dc93512be56444fcab0122aaeae411
Maxim Schuckin <shuckin@mail.ru>
2021-08-14 18:03:09 +0300

    settings  project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: "#8058EC",
+      gray: {
+        light: "#F2F2FA",
+        DEFAULT: "#9D9CA1",
+        dark: "#5A595E",
+      },
+      secondary: {
+        one: "#7D93F8",
+        two: "#5EB6A8",
+        three: "#E8EBF2",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
